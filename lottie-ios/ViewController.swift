e37ff065b8b6e0351fcf2e5d3d97c0b2260a8019
duytc <duytc@vnpay.vn>
2022-12-13 09:04:59 +0700

    Create demo project

---
+++ lottie-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottie-ios
+//
+//  Created by Duy Tran on 13/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
